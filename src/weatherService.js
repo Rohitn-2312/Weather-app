7955b8c5c7ccb98d55df7ed4e1ef405a447a5596
Rohit Nayak <110717323+Rohitn-2312@users.noreply.github.com>
2024-06-09 21:49:43 +0530

    Add files via upload

---
+++ src/weatherService.js
@@ -0,0 +1,30 @@
+const API_KEY = "a88ecf6818bcd9a1cfe3a1823463494d";
+const makeIconURL=(iconId)=>`https://openweathermap.org/img/wn/${iconId}@2x.png`
+const getFormattedWeatherData = async (city, units = "metrics") => {
+  const URL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=${units}`;
+  const data = await fetch(URL)
+    .then((res) => res.json())
+    .then((data) => data);
+  const {
+    weather,
+    main: { temp, feels_like, temp_min, temp_max, pressure, humidity },
+    wind: { speed },
+    sys: { country },
+    name,
+  } = data;
+  const { description, icon } = weather[0];
+  return {
+    description,
+    iconURL:makeIconURL(icon),
+    temp,
+    feels_like,
+    temp_min,
+    temp_max,
+    pressure,
+    humidity,
+    speed,
+    country,
+    name,
+  };
+};
+export { getFormattedWeatherData };
